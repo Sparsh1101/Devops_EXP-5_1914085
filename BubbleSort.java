69818306c4743ee8df3658aea7c28cb275af9fb1
Sparsh1101 <sparshtgupta@gmail.com>
2022-10-19 13:55:52 +0530

    Changed input array

--- BubbleSort.java
+++ BubbleSort.java
@@ -14,7 +14,7 @@ public class BubbleSort {
     }
 
     public static void main(String[] args) {
-        int arr[] = { 3, 60, 35, 2, 45, 320, 5 };
+        int arr[] = { 2, 4, 1, 8, 6, 3, 5, 7 };
         System.out.println("Array Before Bubble Sort");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
